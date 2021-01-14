f515ae8d7cf82c23a74a9554d8a6d364efdc8fc7
Nishant Mittal <mittalnishant14@outlook.com>
2021-01-14 23:31:19 +0530

    chore: update dependecies

--- api/permissions.py
+++ api/permissions.py
@@ -1,13 +1,3 @@
 from rest_framework import permissions
 
-# Sample Permission
-
-# class AllowValidUser(permissions.BasePermission):
-#     message = "You need to register first"
-
-#     def has_permission(self,request,view):
-#         profile = Profile.objects.filter(user=request.user)
-#         if profile:
-#             return True
-#         else:
-#             return False
+# Custom Permissions
